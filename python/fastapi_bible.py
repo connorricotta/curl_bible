190ac84aaf8bebe1ac33ed0f1db893376c481ee2
Connor Ricotta <103922085+connorricotta@users.noreply.github.com>
2022-12-23 04:34:46 +0000

    Added option to discern requests from the docs page and requests from regular web browser.

--- python/fastapi_bible.py
+++ python/fastapi_bible.py
@@ -2,22 +2,60 @@
 from logging import basicConfig, critical, warning
 from os import getenv, mkdir, path
 from pathlib import Path
-from re import search
 from random import choice, randint
+from re import search
+from typing import Union
 
 # Module Imports
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Query, Response, status
 from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
 from fastapi.responses import PlainTextResponse
+from fastapi.staticfiles import StaticFiles
 from mysql.connector import Error, connect
 from starlette import requests
-from typing import Union
 
 # Sub-directory imports
 from config import *
 
-app = FastAPI()
+"""
+Ship the entire swagger/redoc stack to ensure that requests from docs are appended
+with a header 'X-Options': 'docs'. This allows nginx to tell which requests are from the 
+docs, and which are regular web browsers (as they have the same User Agent)
+"""
+app = FastAPI(docs_url=None, redoc_url=None)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css",
+    )
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - ReDoc",
+        redoc_js_url="/static/redoc.standalone.js",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
 
 # Send plain-text responses to error messages instead of JSON.
 @app.exception_handler(RequestValidationError)
