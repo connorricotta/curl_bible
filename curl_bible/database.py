8ab777d68877ae90ba8e08c8119180a40140c9de
Connor Ricotta <connorricotta@noreply.ricotta.dev>
2023-08-16 01:10:13 -0400

    container can't connect to DB

--- curl_bible/database.py
+++ curl_bible/database.py
@@ -18,8 +18,10 @@ db_port = getenv("MYSQL_DB_PORT")
 try:
     s = getaddrinfo(db_host, 3306, proto=IPPROTO_TCP)
     db_host = s[-1][-1][0]
+    print(f"Got db_host of {db_host} ")
 except Exception:
     db_host = "192.168.0.10"
+    print(f"New db_host is {db_host} ")
 
 SQLALCHEMY_DATABASE_URL = f"mariadb+mariadbconnector://{username}:{password}@{db_host}/{db_name}?charset=utf8mb4"
 
@@ -28,6 +30,7 @@ try:
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 except Exception:
     db_host = "localhost"
+    print(f"Couldn't connect, new db_host is {db_host}")
     SQLALCHEMY_DATABASE_URL = f"mariadb+mariadbconnector://{username}:{password}@{db_host}/{db_name}?charset=utf8mb4"
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- curl_bible/docker-start.sh
+++ curl_bible/docker-start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:10000 -k uvicorn.workers.UvicornWorker curl_bible.server:app 
-# --log-level warning --error-logfile error.log --capture-output --log-config logging.conf
+gunicorn --bind 0.0.0.0:10000 -k uvicorn.workers.UvicornWorker curl_bible.server:app --log-level info --error-logfile error.log --capture-output --log-config logging.conf
