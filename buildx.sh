3a74f0837dc7402d42b3aa37f21f7c95849e5e66
Connor Ricotta <connorricotta@noreply.ricotta.dev>
2023-12-22 02:16:21 -0500

    Total revamp of directory structure + new db image

--- buildx.sh
+++ buildx.sh
@@ -1,3 +1,4 @@
 TAG=$(cat .bumpversion.cfg | grep 'current_version =' | awk -F "=" '{ print $2}' | xargs)
 echo v$TAG
+
 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64  --cache-from type=registry,ref=quay.io/connorricotta/curl_bible --cache-from type=registry,ref=quay.io/connorricotta/curl_bible,mode=max -t quay.io/connorricotta/curl_bible:v$TAG -t quay.io/connorricotta/curl_bible:latest --push .
--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ sed -i "s/MYSQL_ROOT_PASSWORD=.*$/MYSQL_ROOT_PASSWORD=$DB_ROOT_PASS/g" .env
 
 # Copy it inside the docker folder
 cp .env curl_bible/.env
+cp .env bible_db/.env
 echo -e "\e[32mFiles successfully modified!\e[0m"
 echo -e "Regular DB User is: \e[1m$DB_USER_PASS\e[0m\nRoot DB User is:    \e[1m$DB_ROOT_PASS\e[0m"
