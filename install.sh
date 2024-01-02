f2593d93d0f1dd6e667fd3815ce515b67bd13f4a
Connor Ricotta <connorricotta@noreply.ricotta.dev>
2024-01-02 12:44:55 -0500

    Remove old files

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sed -i "s/MYSQL_PASSWORD=.*$/MYSQL_PASSWORD=$DB_USER_PASS/g" .env
 sed -i "s/MYSQL_ROOT_PASSWORD=.*$/MYSQL_ROOT_PASSWORD=$DB_ROOT_PASS/g" .env
 
 # Copy it inside the docker folder
-cp .env curl_bible/.env
-cp .env bible_db/.env
+ln -fP .env curl_bible/
+ln -fP .env bible_db/
 echo -e "\e[32mFiles successfully modified!\e[0m"
-echo -e "Regular DB User is: \e[1m$DB_USER_PASS\e[0m\nRoot DB User is:    \e[1m$DB_ROOT_PASS\e[0m"
+echo -e "Regular DB user password is: \e[1m$DB_USER_PASS\e[0m\nRoot DB user password is:    \e[1m$DB_ROOT_PASS\e[0m"
